9ac039b0c435b7fb25d4f6f5f4b9ad9fb7ae6abb
Joy Ame <joyobasame@gmail.com>
2023-08-13 03:05:33 +0100

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggle=document.querySelector('.toggle')
+const open=document.querySelector('.open')
+
+toggle.addEventListener('click',function(){
+    if('clicked'){
+        open.style.display='block'
+    }else if('clicked')
+    {
+        open.style.display='none'
+    }
+})
